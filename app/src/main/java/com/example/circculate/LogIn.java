1b824f815929202d832188d4cb9b1b242661ea40
honghanmyb <han.hong@mail.utoronto.ca>
2019-02-26 23:49:05 -0500

    Add sign up and log in

--- app/src/main/java/com/example/circculate/LogIn.java
+++ app/src/main/java/com/example/circculate/LogIn.java
@@ -2,50 +2,89 @@ package com.example.circculate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LogIn extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private TextInputEditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-
-
-//        FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-        // FloatingActionButton fab = findViewById(R.id.fab);
-        // fab.setOnClickListener(new View.OnClickListener() {
-        //     @Override
-        //     public void onClick(View view) {
-        //         Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-        //                 .setAction("Action", null).show();
-        //     }
-        // });
-
-
+        mAuth = FirebaseAuth.getInstance();
 
     }
 
-    public void GoToHome(View view) {
-         Intent intent = new Intent(this, HomePage.class);
-         startActivity(intent);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(mAuth.getCurrentUser() != null){
+            toHomeActivity();
+        }
     }
 
-    public void GoToSignup(View view) {
+    public void loginToHome(View view){
+        if(verifyEmailPassword()){
+            //start log in process
+            if(email != null && password != null){
+                mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    toHomeActivity();
+                                }else {
+                                    showToast("Invalid email or password");
+                                }
+                            }
+                        });
+            }
+        }
+    }
+
+    public void toSignupActivity(View view) {
         Intent intent = new Intent(this, SignUp.class);
         startActivity(intent);
     }
+
+    private void toHomeActivity() {
+
+        Intent intent = new Intent(this, HomePage.class);
+        startActivity(intent);
+    }
+
+    private boolean verifyEmailPassword(){
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        if(TextUtils.isEmpty(email.getText().toString())){
+            email.setError("Required");
+            return false;
+        }
+        if(TextUtils.isEmpty(password.getText().toString())){
+            password.setError("Required");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void showToast(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
 }
