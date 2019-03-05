b04306fe109ff786534c5ac40d042680529bc06b
Yixuan Zhang <zhang4yu@mail.uc.edu>
2019-03-05 15:38:55 -0500

    tt

---
+++ app/src/main/java/com/example/circculate/EventDecorator.java
@@ -0,0 +1,33 @@
+package com.example.circculate;
+
+import android.app.usage.UsageEvents;
+import android.graphics.Color;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.spans.DotSpan;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class EventDecorator implements DayViewDecorator {
+    private int color;
+    private HashSet<CalendarDay> dates;
+
+    public EventDecorator(Collection <CalendarDay> dates){
+        this.color = Color.parseColor("#FF4081");
+        this.dates = new HashSet<>(dates);
+    }
+
+    @Override
+    public boolean shouldDecorate(CalendarDay calendarDay) {
+        return dates.contains(calendarDay);
+    }
+
+    @Override
+    public void decorate(DayViewFacade dayViewFacade) {
+        dayViewFacade.addSpan(new DotSpan(5, color));
+
+    }
+}
