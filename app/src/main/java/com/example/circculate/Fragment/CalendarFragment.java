66470b43df72b003a8557d99ea3229b9ed978179
Yixuan Zhang <zhang4yu@mail.uc.edu>
2019-03-05 14:54:08 -0500

    Update CalendarFragment.java

--- app/src/main/java/com/example/circculate/Fragment/CalendarFragment.java
+++ app/src/main/java/com/example/circculate/Fragment/CalendarFragment.java
@@ -88,13 +88,6 @@ public class CalendarFragment extends Fragment implements SwipeRefreshLayout.OnR
                 if(task.isSuccessful()){
 //                    List<DocumentSnapshot> documentSnapshot = task.getResult().getDocuments();
                     for(QueryDocumentSnapshot doc:task.getResult()){
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
                         int monthofDay;
                         int month;
                         int year;
@@ -127,41 +120,7 @@ public class CalendarFragment extends Fragment implements SwipeRefreshLayout.OnR
                     }
                     eventCalendar = (MaterialCalendarView) getView().findViewById(R.id.calendarView);
                     eventCalendar.addDecorator(eventDecorator);
-=======
-                        Log.d("fresh", doc.getData().toString());
 
-                    }
->>>>>>> parent of 77a3e03... dot is not displayed
-=======
-                        Log.d("fresh", doc.getData().toString());
-
-                    }
->>>>>>> parent of 77a3e03... dot is not displayed
-=======
-                        Log.d("fresh", doc.getData().toString());
-
-                    }
->>>>>>> parent of 77a3e03... dot is not displayed
-=======
-                        Log.d("fresh", doc.getData().toString());
-
-                    }
->>>>>>> parent of 77a3e03... dot is not displayed
-=======
-                        Log.d("fresh", doc.getData().toString());
-
-                    }
->>>>>>> parent of 77a3e03... dot is not displayed
-=======
-                        Log.d("fresh", doc.getData().toString());
-
-                    }
->>>>>>> parent of 77a3e03... dot is not displayed
-=======
-                        Log.d("fresh", doc.getData().toString());
-
-                    }
->>>>>>> parent of 77a3e03... dot is not displayed
 
                 }
 
