f68d35a66667990504a3ac7157d646a47e4d8434
Yixuan Zhang <zhang4yu@mail.uc.edu>
2019-03-05 14:49:20 -0500

    Update CalendarFragment.java

--- app/src/main/java/com/example/circculate/Fragment/CalendarFragment.java
+++ app/src/main/java/com/example/circculate/Fragment/CalendarFragment.java
@@ -92,6 +92,7 @@ public class CalendarFragment extends Fragment implements SwipeRefreshLayout.OnR
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
                         int monthofDay;
                         int month;
                         int year;
@@ -124,6 +125,11 @@ public class CalendarFragment extends Fragment implements SwipeRefreshLayout.OnR
                     }
                     eventCalendar = (MaterialCalendarView) getView().findViewById(R.id.calendarView);
                     eventCalendar.addDecorator(eventDecorator);
+=======
+                        Log.d("fresh", doc.getData().toString());
+
+                    }
+>>>>>>> parent of 77a3e03... dot is not displayed
 =======
                         Log.d("fresh", doc.getData().toString());
 
