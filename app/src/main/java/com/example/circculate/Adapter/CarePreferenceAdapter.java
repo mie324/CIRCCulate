6085b16a56c48b2cb624b3f65bcdca90d3c3480f
honghanmyb <han.hong@mail.utoronto.ca>
2019-03-31 22:20:04 -0400

    Add care preference display

--- app/src/main/java/com/example/circculate/Adapter/CarePreferenceAdapter.java
+++ app/src/main/java/com/example/circculate/Adapter/CarePreferenceAdapter.java
@@ -2,27 +2,60 @@ package com.example.circculate.Adapter;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatEditText;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ViewSwitcher;
 
 import com.example.circculate.Model.CarePreferenceAnswerModel;
 import com.example.circculate.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.List;
 
 public class CarePreferenceAdapter extends RecyclerView.Adapter<CarePreferenceAdapter.PreferenceViewHolder> {
     private Context ctx;
-    private List<CarePreferenceAnswerModel> carePreferences;
+    private List<String> answers;
+    private FirebaseAuth mAuth;
+    private String[] titleArray = {"What do I need to think about or do before I feel ready to have the conversation?",
+            "What makes my life meaningful?",
+            "What do I value most?",
+            "What are the three most important things that I want my SDM, family, friends and/or health care providers to understand about my future personal or health care wishes?",
+            "What concerns do I have about how my health may change in the future?",
+            "Other thoughts:"};
+    private String[] desArray = {
+            "",
+            "e.g. time with family or friends, faith, love for garden, music, art, work, hobbies, pet",
+            "e.g. live independently, make my own decisions, enjoy a good meal, have my privacy upheld, recognize or talk with others",
+            "",
+            "",
+            ""
+    };
+    private String[] constructArray;
+    private static final String PATIENT_ID = "rUSzZ8NTnthxkk36ItXBRGWRkRr2";
+    private FirebaseFirestore db;
+    private String username;
 
-    public CarePreferenceAdapter(Context ctx, List<CarePreferenceAnswerModel> carePreferences){
+    public CarePreferenceAdapter(Context ctx, List<String> answers, String username){
         this.ctx = ctx;
-        this.carePreferences = carePreferences;
-
+        this.answers = answers;
+        this.username = username;
+        db = FirebaseFirestore.getInstance();
+        constructArray = new String[answers.size()];
+        mAuth = FirebaseAuth.getInstance();
+        for(int i = 0; i < answers.size(); i++){
+            constructArray[i] = answers.get(i);
+        }
     }
 
     @NonNull
@@ -34,35 +67,75 @@ public class CarePreferenceAdapter extends RecyclerView.Adapter<CarePreferenceAd
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CarePreferenceAdapter.PreferenceViewHolder holder, int position) {
-        CarePreferenceAnswerModel carePreference = carePreferences.get(position);
-        final String username = carePreference.getUsername();
-        holder.usernameHolder.setText(username);
-        holder.typeIndicator.setText("'s care preference");
-        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+    public void onBindViewHolder(@NonNull final CarePreferenceAdapter.PreferenceViewHolder holder, final int position) {
+        final String userAnswer = answers.get(position);
+        holder.title.setText(titleArray[position]);
+        holder.description.setText(desArray[position]);
+        holder.patientAnswer.setText(userAnswer);
+
+        holder.switcher.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(ctx, "Now show detail of " + username, Toast.LENGTH_SHORT).show();
+                if(mAuth.getUid().equals(PATIENT_ID)){
+                    // only patient can edit answers
+                    if(!holder.isHolderEditable){
+                        holder.isHolderEditable = true;
+                        holder.switcher.showNext();
+                        holder.editText.setText(userAnswer);
+                    }
+                }else {
+                    Toast.makeText(ctx, "Only the patient can edit answers", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
+        holder.submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //if not editable, then the button will no show
+                if(holder.isHolderEditable){
+                    final String newAnswer = holder.editText.getText().toString();
+                    constructArray[position] = newAnswer;
+                    CarePreferenceAnswerModel newPreference = new CarePreferenceAnswerModel(mAuth.getUid(),
+                            username, constructArray);
+                    db.collection("carepreferences").document(newPreference.getUserId())
+                            .set(newPreference).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                answers.set(position, newAnswer);
+                                notifyDataSetChanged();
+                                holder.switcher.showPrevious();
+                                holder.isHolderEditable = false;
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return carePreferences.size();
+        return answers.size();
     }
 
 
     public class PreferenceViewHolder extends RecyclerView.ViewHolder{
-        public TextView usernameHolder, typeIndicator;
-        public LinearLayout parentLayout;
+        public TextView title, description, patientAnswer;
+        public ViewSwitcher switcher;
+        public AppCompatEditText editText;
+        public AppCompatButton submitBtn;
+        public boolean isHolderEditable = false;
 
         public PreferenceViewHolder(View itemView){
             super(itemView);
-            usernameHolder = itemView.findViewById(R.id.username);
-            typeIndicator = itemView.findViewById(R.id.type_indicator);
-            parentLayout = itemView.findViewById(R.id.parent_layout);
+            title = itemView.findViewById(R.id.title);
+            description = itemView.findViewById(R.id.description);
+            patientAnswer = itemView.findViewById(R.id.answer_textview);
+            switcher = itemView.findViewById(R.id.switcher);
+            editText = itemView.findViewById(R.id.answer_edittext);
+            submitBtn = itemView.findViewById(R.id.answer_submit_btn);
         }
 
     }
--- app/src/main/java/com/example/circculate/Fragment/CarePreferenceFragment.java
+++ app/src/main/java/com/example/circculate/Fragment/CarePreferenceFragment.java
@@ -5,18 +5,24 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.AppCompatEditText;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 import com.example.circculate.Adapter.CarePreferenceAdapter;
 import com.example.circculate.Model.CarePreferenceAnswerModel;
+import com.example.circculate.Model.UserModel;
 import com.example.circculate.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -29,10 +35,19 @@ import java.util.List;
  */
 public class CarePreferenceFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
     private static final String TAG = "CarePreference";
-    private ArrayList<CarePreferenceAnswerModel> carePreferences;
-    private RecyclerView recyclerView;
+    private static final String PATIENT_ID = "rUSzZ8NTnthxkk36ItXBRGWRkRr2";
+//    private ArrayList<CarePreferenceAnswerModel> carePreferences;
+//    private RecyclerView recyclerView;
     private FirebaseFirestore db;
-    private CarePreferenceAdapter carePreferenceAdapter;
+    private CarePreferenceAnswerModel patientPreference;
+    private CarePreferenceAdapter preAdapter;
+    private ArrayList<String> answers;
+    private RecyclerView recyclerView;
+    private FirebaseAuth mAuth;
+    private String username;
+    private static final int MAX_ANSWERS = 6;
+
+//    private CarePreferenceAdapter carePreferenceAdapter;
 
     public CarePreferenceFragment() {
         // Required empty public constructor
@@ -48,33 +63,77 @@ public class CarePreferenceFragment extends Fragment implements SwipeRefreshLayo
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_care_preference, container, false);
-        recyclerView = root.findViewById(R.id.care_pre_recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+//        recyclerView = root.findViewById(R.id.care_pre_recycler_view);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
         db = FirebaseFirestore.getInstance();
-        getAllCarePreference();
+        mAuth = FirebaseAuth.getInstance();
+        recyclerView = root.findViewById(R.id.pre_recycler);
+        getPatientCarePreference(root);
 
         return root;
     }
 
-    private void getAllCarePreference(){
-        db.collection("carepreferences").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if(task.isSuccessful()){
-                    displayCarePreferences(task.getResult().getDocuments());
-                }
-            }
-        });
+    private void getPatientCarePreference(final View root){
+        db.collection("carepreferences").document(PATIENT_ID).get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if(task.isSuccessful()){
+                            patientPreference = task.getResult().toObject(CarePreferenceAnswerModel.class);
+
+                            setupRecyclerView(root);
+                        }
+                    }
+                });
     }
 
-    private void displayCarePreferences(List<DocumentSnapshot> carePreferenceDocs){
-        carePreferences = new ArrayList<>();
-        for(DocumentSnapshot doc: carePreferenceDocs){
-            carePreferences.add(doc.toObject(CarePreferenceAnswerModel.class));
-        }
+    private void setupRecyclerView(View root){
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+        answers = new ArrayList<>();
+        answers.add(patientPreference.getAnswer0());
+        answers.add(patientPreference.getAnswer1());
+        answers.add(patientPreference.getAnswer2());
+        answers.add(patientPreference.getAnswer3());
+        answers.add(patientPreference.getAnswer4());
+        answers.add(patientPreference.getAnswer5());
+        db.collection("users").document(mAuth.getUid()).get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if(task.isSuccessful()){
+                            UserModel user = task.getResult().toObject(UserModel.class);
+                            username = user.getUsername();
+                            preAdapter = new CarePreferenceAdapter(getActivity(), answers, username);
+                            recyclerView.setAdapter(preAdapter);
+                        }
+                    }
+                });
+//        preAdapter = new CarePreferenceAdapter(getActivity(), answers);
+//        recyclerView.setAdapter(preAdapter);
 
-        carePreferenceAdapter = new CarePreferenceAdapter(getActivity(), carePreferences);
-        recyclerView.setAdapter(carePreferenceAdapter);
     }
+
+
+
+//    private void getAllCarePreference(){
+//        db.collection("carepreferences").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                if(task.isSuccessful()){
+//                    displayCarePreferences(task.getResult().getDocuments());
+//                }
+//            }
+//        });
+//    }
+//
+//    private void displayCarePreferences(List<DocumentSnapshot> carePreferenceDocs){
+//        carePreferences = new ArrayList<>();
+//        for(DocumentSnapshot doc: carePreferenceDocs){
+//            carePreferences.add(doc.toObject(CarePreferenceAnswerModel.class));
+//        }
+//
+//        carePreferenceAdapter = new CarePreferenceAdapter(getActivity(), carePreferences);
+//        recyclerView.setAdapter(carePreferenceAdapter);
+//    }
 
 }
